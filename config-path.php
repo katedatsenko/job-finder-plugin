6f6c07cf2b73ffbaef3f742c0fbf21b8fb2c02bb
Kate Datsenko <kate.datsenko@gmail.com>
2017-02-02 23:29:55 +0200

    Определение путей: установка констант

--- config-path.php
+++ config-path.php
@@ -5,3 +5,18 @@
  * Date: 002 02.02.17
  * Time: 23:01
  */
+define("JOBFINDER_PlUGIN_DIR", plugin_dir_path(__FILE__));
+define("JOBFINDER_PlUGIN_URL", plugin_dir_url( __FILE__ ));
+define("JOBFINDER_PlUGIN_SLUG", preg_replace( '/[^\da-zA-Z]/i', '_',  basename(JOBFINDER_PlUGIN_DIR)));
+define("JOBFINDER_PlUGIN_TEXTDOMAIN", str_replace( '_', '-', JOBFINDER_PlUGIN_SLUG ));
+define("JOBFINDER_PlUGIN_OPTION_VERSION", JOBFINDER_PlUGIN_SLUG.'_version');
+define("JOBFINDER_PlUGIN_OPTION_NAME", JOBFINDER_PlUGIN_SLUG.'_options');
+define("JOBFINDER_PlUGIN_AJAX_URL", admin_url('admin-ajax.php'));
+
+    if ( ! function_exists( 'get_plugins' ) ) {
+        require_once ABSPATH . 'wp-admin/includes/plugin.php';
+    }
+ $TPOPlUGINs = get_plugin_data(JOBFINDER_PlUGIN_DIR.'/'.basename(JOBFINDER_PlUGIN_DIR).'.php', false, false);
+
+ define("STEPBYSTEP_PlUGIN_VERSION", $TPOPlUGINs['Version']);
+ define("STEPBYSTEP_PlUGIN_NAME", $TPOPlUGINs['Name']);
