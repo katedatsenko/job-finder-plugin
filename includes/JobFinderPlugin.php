8b2bbb319da3dd74f8ff1ea2181e5089c3eef35e
Kate Datsenko <kate.datsenko@gmail.com>
2017-02-02 21:51:15 +0200

    Добавление класса JobFinderPlugin

---
+++ includes/JobFinderPlugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kate
+ * Date: 002 02.02.17
+ * Time: 21:43
+ */
+
+namespace includes;
+
+class JobFinderPlugin
+{
+    protected function __construct() {
+
+    }
+}
