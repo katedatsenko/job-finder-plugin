21d176010dee08d85b349108c9f77bff19900883
Kate Datsenko <kate.datsenko@gmail.com>
2017-02-09 15:08:55 +0200

    Вызов класса JobFinderMyDashboardMenuController

--- includes/common/JobFinderLoader.php
+++ includes/common/JobFinderLoader.php
@@ -9,6 +9,7 @@
 namespace includes\common;
 use includes\controllers\admin\menu\JobFinderMainAdminMenuController;
 use includes\controllers\admin\menu\JobFinderMainAdminSubMenuController;
+use includes\controllers\admin\menu\JobFinderMyDashboardMenuController;
 use includes\example\JobFinderExampleAction;
 use includes\example\JobFinderExampleFilter;
 
@@ -47,6 +48,7 @@ class JobFinderLoader
 
         JobFinderMainAdminMenuController::newInstance();
         JobFinderMainAdminSubMenuController::newInstance();
+        JobFinderMyDashboardMenuController::newInstance();
     }
 
     /**
