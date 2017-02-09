26114f2610267f8e0a57da2ffce581f46b4783fd
Kate Datsenko <kate.datsenko@gmail.com>
2017-02-09 13:35:49 +0200

    Создадим базовый класс контроллер для создания пунктов меню

---
+++ includes/controllers/admin/menu/JobFinderBaseAdminMenuController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Kate
+ * Date: 009 09.02.17
+ * Time: 13:31
+ */
+
+namespace includes\controllers\admin\menu;
+
+abstract class JobFinderBaseAdminMenuController
+{
+    public function __construct(){
+        /*
+        * Регистрирует хук-событие. При регистрации указывается PHP функция,
+        * которая сработает в момент события, которое вызывается с помощью do_action().
+        */
+        add_action('admin_menu', array( &$this, 'action'));
+    }
+
+    abstract public function action();
+    abstract public function render();
+}
