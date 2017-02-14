15b61981110a4f16a6b94142e184b86cb6e45f36
Kate Datsenko <kate.datsenko@gmail.com>
2017-02-15 01:05:50 +0200

    Поправила css

--- includes/controllers/admin/views/main-admin.php
+++ includes/controllers/admin/views/main-admin.php
@@ -9,7 +9,7 @@ echo '<div class="header">
     <h1>This is temporary view</h1>
         </div>';
 echo '<div class="body">
-        <form>
+        <form class="temporary">
            <input name="name" placeholder="Укажите ваше имя" class="textbox" required />
            <input name="emailaddress" placeholder="Укажите ваш Email" class="textbox" type="email" required />
            <textarea rows="4" cols="50" name="subject" placeholder="Введите ваше сообщение:" class="message" required></textarea>
